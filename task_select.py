eb1972cf8fd6e154c8750c462c1c3897239514f6
davidxiang101 <davidxiang101@gmail.com>
2024-01-11 15:23:25 -0500

    api

---
+++ task_select.py
@@ -0,0 +1,28 @@
+import random
+from notion_client import Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+notion_api_key = os.getenv("NOTION_API_KEY")
+
+notion = Client(auth=notion_api_key)
+
+
+def get_random_task(database_id, status):
+    response = notion.databases.query(
+        **{
+            "database_id": database_id,
+            "filter": {"property": "Status", "select": {"equals": status}},
+        }
+    )
+
+    tasks = response.get("results", [])
+    if tasks:
+        return random.choice(tasks)
+    else:
+        return None
+
+
+random_task = get_random_task("todo", "To Solve")
+print(random_task)
